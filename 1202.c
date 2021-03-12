5612ed55373325075449b62068a10838175811ab
rafapgomes <pimentelgomes17@gmail.com>
2021-03-12 12:23:27 -0300

    Festival das Noites Brancas

--- 1202.c
+++ 1202.c
@@ -1,26 +1,18 @@
-#include <stdio.h>
-int potencia(int cont)
-{
-    int num=1;
-    for(int i=1; i<=cont; i++)
-    {
-        num = num*2;
-    }
-    return num;
-}
 
-int conversao(long long int n)
+
+#include <stdio.h>
+#include <string.h>
+
+
+int conversao(num[])
 {
-    int num=0,cont=0;
-    while(n!=0)
-    {
-        int aux = n%10;
-        aux  = aux * potencia(cont); 
-        num = num+aux;
-        cont = cont+1;
-        n = n/10;
-    }
-    return num;
+	 for (int i = 0; i < strlen(num); i++) 
+	{
+                    ans *= 2;
+                    ans += (num[i] - '0');
+                    ans = ans%1500;
+        }
+        return ans;
 }
 
 
@@ -28,31 +20,47 @@ int fibonnaci(int n)
 {
     int ant=0,aux=0,prox=1,ret;
     int cont=0;
-    while(cont<n-1)
-    { 
+    
+    if(n<2)
+    {
+    	return n;
+    }else{
+    
+     while(cont<=n-2)
+     { 
        aux=ant+prox;
        aux = aux%1000;
        ant=prox;
        prox=aux;
        cont = cont+1;
+     }
     }
     return aux;
 }
 int main()
 {
+
+
     int aux,num2,cont,i=0;
-    long long int num;
+    char num[100001];
+ int ans = 0, ciclo = 1500;
 
       
-      scanf("%d",&cont);
-	for(int i=0;i<cont;i++)
+     
+	scanf("%d",&cont);
+	getchar();
+	for(int i=0; i<cont; i++)
 	{
-		
-	 	scanf("%lld",&num);
- 		printf("%d",fibonnaci(conversao(num)));
+	
+	 scanf("%s",num);
+	    
+    		
+    	       	printf("%03d",fibonnaci(conversao(num));
+	      printf("\n");
 
-         	printf("\n");
-    	 
+
+
+        	
        }
     
             
